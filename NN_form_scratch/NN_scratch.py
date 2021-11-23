a7e1dea09de225f1b419890cbc571d46d830a51f
michal <mm.sikorski3@gmail.com>
2021-11-23 11:01:02 +0100

    Add simple function to class Network

--- NN_form_scratch/NN_scratch.py
+++ NN_form_scratch/NN_scratch.py
@@ -2,13 +2,31 @@ import numpy as np
 
 
 class Network:
-    def __init__(self):
-        pass
+    def __init__(self, size):
+        
+        self.size = size
+        self.num_layers = len(size)
+        
+        self.weights = None
+        self.biases = None
+        #pass
 
     def train():
         pass
     
+    def net_parameters(self):
+        biases = 0
+        weights = 0
+        for i in range(1, self.num_layers):
+            biases += self.size[i]
+            if i > 0 and i < self.num_layers:
+                weights += self.size[i-1]*self.size[i]
+            
+        print("Biases : ", biases)
+        print("Weights : ", weights)
+    
+    
 
 
-if "__main_":
-    net = Network()
+net = Network([784, 10, 10])
+net.net_parameters()
